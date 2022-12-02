e4e13fdcb995ea441366793b2e1496c5252963df
Niols <niols@niols.fr>
2022-12-02 02:10:57 +0100

    Document and cleanup

--- days/1-calories/solution.ml
+++ days/1-calories/solution.ml
@@ -1,24 +1,39 @@
 open Ext
 
-(* Parsing *)
+(** {1 Parsing}
+
+    We represent the elfs as an [int list list]: each elf corresponds to a list
+    of calories/integers and we have a list of elves. We represent the calories
+    as an [int list]: each elf is associated to their total number of
+    calories. *)
 
 let elfs : int list list =
+  (* We use {!Read.lines_until_empty} to get a batch of integers from standard
+     input and to stop on the first empty line. We use {!List.init_until} to
+     repeat this process until we encounter an {!End_of_file}. *)
   List.init_until @@ fun _ ->
   try Some Read.(lines_until_empty int) with End_of_file -> None
 
 let calories : int list = List.map (List.fold_left (+) 0) elfs
 
-(* Part 1 *)
+(* {1 Part 1}
 
-let max : int = List.fold_left max 0 calories
+   We want the maximum number in the list {!calories}. *)
 
-let () = pf "Maximimum amount of calories: %d.@." max
+let () =
+  pf "Maximimum amount of calories: %d.@."
+    (List.fold_left max 0 calories)
 
-(* Part 2 *)
+(* {2 Part 2}
+
+   We want the sum of the three maximal numbers in {!calories}. *)
 
 let max3 =
+  (* NOTE: The inversion of [a] and [b] in the argument of {!List.sort} is on
+     purpose. *)
   match List.sort (fun a b -> Int.compare b a) calories with
   | top1 :: top2 :: top3 :: _ ->
-    pf "Amount of calories in top 3 is: %d.@." (top1 + top2 + top3)
+    pf "Amount of calories in top 3: %d.@." (top1 + top2 + top3)
   | _ ->
-    epf "NOT ENOUGH ELVES!@."
+    epf "NOT ENOUGH ELVES!@.";
+    exit 7
