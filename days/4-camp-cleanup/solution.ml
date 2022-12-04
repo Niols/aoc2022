7462a2c199b5bdc27e4e5521a972d4bb8cdc172c
Niols <niols@niols.fr>
2022-12-04 18:07:15 +0100

    Day 4 Part 1

--- days/4-camp-cleanup/solution.ml
+++ days/4-camp-cleanup/solution.ml
@@ -1,9 +1,31 @@
 (** {1 Day 0 -- Sample} *)
 
-(* open Ext *)
+open Ext
 
 (** {2 Parsing} *)
 
+let section_assignment_pairs : ((int * int) * (int * int)) list =
+  let comma = Str.regexp "," in
+  let dash = Str.regexp "-" in
+  let range = Read.(pair ~sep:dash int int) in
+  Read.(lines_until_empty (pair ~sep:comma range range))
+
 (** {2 Part 1} *)
+
+(** Whether the interval [needle] is included in the interval [haystack]. *)
+let interval_inclusion ~needle:(n1, n2) ~haystack:(h1, h2) =
+  h1 <= n1 && n2 <= h2
+
+(** Whether one of the two intervals is included in the other. *)
+let any_interval_inclusion (n1, n2) (h1, h2) =
+  interval_inclusion ~needle:(n1, n2) ~haystack:(h1, h2)
+  || interval_inclusion ~needle:(h1, h2) ~haystack:(n1, n2)
+
+let () =
+  section_assignment_pairs
+  |> List.map (fun (interval1, interval2) -> any_interval_inclusion interval1 interval2)
+  |> List.filter Fun.id
+  |> List.length
+  |> pf "%d@."
 
 (** {2 Part 2} *)
