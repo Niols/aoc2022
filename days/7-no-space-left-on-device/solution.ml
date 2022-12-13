c98ef2ddcce4c2ba4b38bfb4433ea3b4f343ce70
Niols <niols@niols.fr>
2022-12-13 23:54:37 +0100

    ZIPPER

--- days/7-no-space-left-on-device/solution.ml
+++ days/7-no-space-left-on-device/solution.ml
@@ -1,8 +1,106 @@
 (** {1 Day 7 -- No Space Left On Device} *)
 
-(* open Ext *)
+open Ext
+
+module Path = struct
+  type t = string list
+
+  let cd (path : t) : string -> t = function
+    | "/" -> []
+    | ".." -> List.bd path
+    | fname -> path @ [fname]
+end
 
 (** {2 Parsing} *)
+
+module Filesystem = struct
+  type t = entry list
+  and entry = string * file
+  and file = Directory of entry list | RegularFile of int
+  [@@deriving show {with_path=false}]
+
+  type zipper = {
+    entries : entry list;
+    ancestors : ancestor list
+  }
+  and ancestor = {
+    before : entry list ;
+    filename : string ;
+    after : entry list
+  }
+
+  let zip (filesystem : t) =
+    { entries = filesystem; ancestors = [] }
+
+  let head zipper = zipper.entries
+  let with_head entries zipper = { zipper with entries }
+
+  let up zipper =
+    match zipper.ancestors with
+    | [] -> None
+    | ancestor::ancestors ->
+      Some {
+        ancestors ;
+        entries =
+          (List.rev ancestor.before)
+          @ [(ancestor.filename, Directory zipper.entries)]
+          @ ancestor.after
+      }
+
+  let rec root zipper =
+    match up zipper with
+    | None -> zipper
+    | Some zipper -> root zipper
+
+  let unzip zipper = head (root zipper)
+
+  let down zipper target_filename =
+    let rec down_assoc before = function
+      | [] -> None
+      | ((filename, _) as entry) :: entries when filename <> target_filename ->
+        down_assoc (entry :: before) entries
+      | (_, RegularFile _) :: _ -> None
+      | (filename, Directory entries) :: after ->
+        Some {
+          entries ;
+          ancestors = { before ; filename ; after } :: zipper.ancestors
+        }
+    in
+    down_assoc [] zipper.entries
+
+  let cd zipper = function
+    | "/" -> Some (root zipper)
+    | ".." -> up zipper
+    | filename -> down zipper filename
+end
+
+let rec apply_lines zipper =
+  match Read.(line string) with
+  | exception End_of_file -> zipper
+  | line when String.sub line 0 2 = "$ " ->
+    (
+      match String.sub line 2 2 with
+      | "cd" -> apply_lines @@ Option.get @@ Filesystem.cd zipper @@ String.(sub line 5 (length line - 5))
+      | "ls" -> apply_lines zipper
+      | _ -> assert false
+    )
+  | line ->
+    (
+      let (dir_or_size, filename) = Read.(of_string (pair string string)) line in
+      let entry =
+        (filename,
+         if dir_or_size = "dir"
+         then Filesystem.Directory []
+         else Filesystem.RegularFile (ios dir_or_size))
+      in
+      let head = Filesystem.head zipper in
+      assert (not @@ List.mem_assoc filename head);
+      apply_lines @@ Filesystem.with_head (entry :: head) zipper
+    )
+
+let filesystem = Filesystem.unzip @@ apply_lines @@ Filesystem.zip []
+
+let () = epf "%a@." Filesystem.pp filesystem
 
 (** {2 Part 1} *)
 
